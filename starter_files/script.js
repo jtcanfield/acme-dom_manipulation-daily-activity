a3a8e32fec7253e01f14a4d81946fb5663a6452c
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-31 10:42:41 -0400

    add portfolio content

--- starter_files/script.js
+++ starter_files/script.js
@@ -32,7 +32,34 @@ parentHeroElement.appendChild( theParagraphUnderHero );
 /* 2. Add Portfolio Content ************************/
 /***************************************************/
 let parentPortfolioElement = document.getElementById("portfolio");
+let unorderedList = document.createElement( "ul" );
+parentPortfolioElement.appendChild( unorderedList );
+let listItem1 = document.createElement('li');
+let listItem2 = document.createElement('li');
+let listItem3 = document.createElement('li');
+let listItem4 = document.createElement('li');
 
+let h2element1 = document.createElement('h2');
+h2element1.textContent = 'Boats';
+listItem1.appendChild( h2element1 );
+let h2element2 = document.createElement('h2');
+h2element2.textContent = 'Hair Styles';
+listItem2.appendChild( h2element2 );
+let h2element3 = document.createElement('h2');
+h2element3.textContent = 't Shirts';
+listItem3.appendChild( h2element3 );
+let h2element4 = document.createElement('h2');
+h2element4.textContent = 'Dirt';
+listItem4.appendChild( h2element4 );
+
+let portfolioImage1 = document.createElement('img');
+listItem1.appendChild( portfolioImage1 );
+let portfolioImage2 = document.createElement('img');
+listItem2.appendChild( portfolioImage2 );
+let portfolioImage3 = document.createElement('img');
+listItem3.appendChild( portfolioImage3 );
+let portfolioImage4 = document.createElement('img');
+listItem4.appendChild( portfolioImage4 );
 
 
 /***************************************************/
