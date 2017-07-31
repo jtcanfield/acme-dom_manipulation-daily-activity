a2d7fa9b6f39b05074948c21bb3199f24582acad
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-31 11:12:38 -0400

    created loop

--- starter_files/script.js
+++ starter_files/script.js
@@ -31,41 +31,19 @@ parentHeroElement.appendChild( theParagraphUnderHero );
 /***************************************************/
 /* 2. Add Portfolio Content ************************/
 /***************************************************/
-let parentPortfolioElement = document.querySelector('#portfolio > ul');
-let listItem1 = document.createElement('li');
-parentPortfolioElement.appendChild( listItem1 );
-let listItem2 = document.createElement('li');
-parentPortfolioElement.appendChild( listItem2 );
-let listItem3 = document.createElement('li');
-parentPortfolioElement.appendChild( listItem3 );
-let listItem4 = document.createElement('li');
-parentPortfolioElement.appendChild( listItem4 );
+for (i = 0; i < portfolioData.length; i++){
+  let parentPortfolioElement = document.querySelector('#portfolio > ul');
+  let createli = document.createElement("li");
+  let createimgElement = document.createElement("img");
+  createimgElement.setAttribute("src", portfolioData[i].img)
+  createli.appendChild(createimgElement);
+  let createH2Element = document.createElement("h2");
+  createH2Element.textContent = (portfolioData[i].h2);
+  createli.appendChild(createH2Element);
+  parentPortfolioElement.appendChild(createli);
+}
 
-let portfolioImage1 = document.createElement('img');
-portfolioImage1.setAttribute("src", portfolioData[0].img);
-listItem1.appendChild( portfolioImage1 );
-let portfolioImage2 = document.createElement('img');
-portfolioImage2.setAttribute("src", portfolioData[1].img);
-listItem2.appendChild( portfolioImage2 );
-let portfolioImage3 = document.createElement('img');
-portfolioImage3.setAttribute("src", portfolioData[2].img);
-listItem3.appendChild( portfolioImage3 );
-let portfolioImage4 = document.createElement('img');
-portfolioImage4.setAttribute("src", portfolioData[3].img);
-listItem4.appendChild( portfolioImage4 );
 
-let h2element1 = document.createElement('h2');
-h2element1.textContent = (portfolioData[0].h2);
-listItem1.appendChild( h2element1 );
-let h2element2 = document.createElement('h2');
-h2element2.textContent = (portfolioData[1].h2);
-listItem2.appendChild( h2element2 );
-let h2element3 = document.createElement('h2');
-h2element3.textContent = (portfolioData[2].h2);
-listItem3.appendChild( h2element3 );
-let h2element4 = document.createElement('h2');
-h2element4.textContent = (portfolioData[3].h2);
-listItem4.appendChild( h2element4 );
 
 /***************************************************/
 /* 3. Navigation Dropdown **************************/
